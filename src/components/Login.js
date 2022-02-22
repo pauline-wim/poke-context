71009ee7f979177de366cc975ddc01f1febaa220
pauline-wim <linette.wim@hotmail.com>
2022-02-22 17:30:14 +0100

    useForm -> create form on Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,31 @@
 import {} from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
       <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          {...register("username", { required: true, maxLength: 15 })}
+        />
+        {errors.username && <span>Please enter a username</span>}
+        <input
+          type="password"
+          {...register("password", { required: true, maxLength: 6 })}
+        />
+        {errors.password && <span>Please enter a password</span>}
+        <input type="submit" />
+      </form>
     </div>
   );
 }
