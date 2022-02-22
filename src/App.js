27799a832f78580a4e8ea409542a13751abc4b42
pauline-wim <linette.wim@hotmail.com>
2022-02-22 14:19:06 +0100

    Add pages Home & Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+// Components
+import Home from "./components/Home";
+import Login from "./components/Login";
+// CSS
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <h1>Poke</h1>
+      <BrowserRouter>
+        <Header>
+          <h1>Poke</h1>
+          <Navbar>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+          </Navbar>
+        </Header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
+
+const Navbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  width: 10vw;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 30px;
+  background: pink;
+`;
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import {} from "react-router-dom";
+
+function Login() {
+  return (
+    <div className="App">
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Login;
